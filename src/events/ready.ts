fbc9a466f6e3b5f868986e72b5a76555328e3395
Matthew1177 <Matthew1177@users.noreply.github.com>
2020-08-12 22:07:08 -0700

    Added event handler
    Added event handler which allows events in multiple files.

---
+++ src/events/ready.ts
@@ -0,0 +1,9 @@
+import Discord from 'discord.js';
+module.exports = {
+    'disabled': false,
+    'once': false,
+    'run': function (client:Discord.Client) {
+        // @ts-ignore
+        console.log(`Logged in as ${client.user.tag}`)
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,30 @@
+// .env
+import dotenv from 'dotenv';
+dotenv.config();
+// Filesystem
+import fs from 'fs';
+// DiscordJS
+import Discord from 'discord.js';
+const client = new Discord.Client();
+Discord.Constants.Events
+// Load Event Handlers
+fs.readdir('./dist/events/', (err, files) => {
+    if (err) return console.error(err);
+    files.forEach(file =>{
+        const event = require(`./events/${file}`);
+        if (event.disabled) return;
+
+        const eventName = file.split('.')[0];
+        
+        if (eventName) {
+            try {
+                // @ts-ignore
+                client[event.once ? 'once' : 'on'](eventName, (...args) => event.run(client,...args)); 
+            } catch (error) {
+                return;
+            }
+        }
+    }) 
+})
+
+client.login(process.env.TOKEN);
