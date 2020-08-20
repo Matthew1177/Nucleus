d8704a9ce7fb354d3c501e5d4b40a6916a2fd646
Matthew1177 <56777713+Matthew1177@users.noreply.github.com>
2020-08-19 20:49:29 -0700

    Finish command loader

--- src/handlers/CommandHandler.ts
+++ src/handlers/CommandHandler.ts
@@ -1,6 +1,7 @@
 import { Command, NucleusClient } from "../lib";
 import { Collection } from "discord.js";
 import { readdir } from "fs";
+import { basename, join } from "path";
 
 export default class CommandHandler extends Collection<string, Command> {
     client: NucleusClient;
@@ -12,7 +13,15 @@ export default class CommandHandler extends Collection<string, Command> {
         
         readdir(dir, (err,files) => {
             if (err) return console.error(err);
-            
+
+            files.forEach(file => {
+                const name = basename(file).split(".").slice(0, -1).join(".");
+                // eslint-disable-next-line @typescript-eslint/no-var-requires
+                const Command = ((r) => r.default || r)(require(`${join(dir,file)}`));
+                const command = new Command(client, name);
+
+                this.set(name, command);
+            });
         });
     }
 }
