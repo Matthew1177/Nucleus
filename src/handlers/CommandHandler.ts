0d9ceda69f48354b95a98e36b670622a1cfba873
Matthew1177 <56777713+Matthew1177@users.noreply.github.com>
2020-08-20 20:23:31 -0700

    add logs in command handler

--- src/handlers/CommandHandler.ts
+++ src/handlers/CommandHandler.ts
@@ -12,30 +12,43 @@ export default class CommandHandler extends Collection<string, Command> {
 
         this.client = client;
         
+        console.log(`Loading modules from '${dir}'`);
+
+        let numMods = 0;
+        let numCmds = 0;
+
         readdirSync(dir).forEach(mod => {
             const cmdsInMod = readdirSync(`${dir}/${mod}/`).filter(f => f.endsWith(".js"));
-            const modName = basename(mod).split(".").slice(0, -1).join(".");
+            const modName = basename(mod);
 
             this.modules.set(modName, new Collection());
             
+            console.log(`Loading Module ${modName}`);
 
             for (const file of cmdsInMod) {
                 const name = basename(file).split(".").slice(0, -1).join(".");
                 // eslint-disable-next-line @typescript-eslint/no-var-requires
-                const req = require(`${dir}/${mod}/${file}`);
-                const newReq = req(client, name);
+                const req = ((r) => r.default || r)(require(`${dir}/${mod}/${file}`));
+                const newReq = new req(client, name);
 
                 this.set(name, newReq);
                 this.modules.get(modName)!.set(name, newReq);
+
+                console.log(`Loaded Command '${name}'`);
+                ++numCmds;
             }
+            ++numMods;
+            console.log(`Loaded Module '${modName}'`);
         });
+
+        console.log(`Loaded ${numMods} Modules and ${numCmds}`);
     }
 
     getCommand (name: string): Command | null {
         if (this.has(name)) return this.get(name) as Command;
 
         const alias = this.find((cmd) => cmd.aliases.includes(name));
-        return alias ?? null;
+        return alias || null;
     }
 
     getModule (name: string): Collection<string,Command> | null {
