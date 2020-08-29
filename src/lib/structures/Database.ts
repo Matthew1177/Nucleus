782f645f09a38da7558c9aa3d2c3530f4c0f17a0
Matthew1177 <56777713+Matthew1177@users.noreply.github.com>
2020-08-29 16:20:54 -0700

    fixes to db

--- src/lib/structures/Database.ts
+++ src/lib/structures/Database.ts
@@ -1,4 +1,4 @@
-import { MongoClient } from "mongodb";
+import { MongoClient, FilterQuery, UpdateOneOptions } from "mongodb";
 import NucleusClient from "../extensions/NucleusClient";
 
 export default class Database {
@@ -22,23 +22,25 @@ export default class Database {
         return val;
     }
 
-    async has(table: string, key: string): Promise<boolean> {
-        const db = await this.mongo.connect();
-        const dbo = db.db(process.env.DATABASE);
-
-        const tab = dbo.collection(table);
-
-        const val = tab.findOne({id: key});
-        return !!val;
-    }
-
-    async insert(table: string, obj: Record<string | number | symbol, unknown>): Promise<void> {
+    // It can be any value, does not matter
+    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+    async insert(table: string, obj: any): Promise<void> {
         const db = await this.mongo.connect();
         const dbo = db.db(process.env.DATABASE);
 
         const tab = dbo.collection(table);
 
         await tab.insertOne(obj);
+        return;
+    }
+
+    async update(table: string, filter: FilterQuery<unknown>, update: UpdateOneOptions): Promise<void> {
+        const db = await this.mongo.connect();
+        const dbo = db.db(process.env.DATABASE);
+
+        const tab = dbo.collection(table);
+
+        await tab.updateOne(filter,update);
         return;
     }
 }
