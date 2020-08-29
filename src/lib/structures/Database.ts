c3352f4c1f88b39df48e96f6bf80f3c8a7e94c92
Matthew1177 <56777713+Matthew1177@users.noreply.github.com>
2020-08-29 16:07:48 -0700

    db rewrite - get & pooling

--- src/lib/structures/Database.ts
+++ src/lib/structures/Database.ts
@@ -1,55 +1,24 @@
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-// Typing this file can cause issues
-
+import { MongoClient } from "mongodb";
 import NucleusClient from "../extensions/NucleusClient";
-import { r, MasterPool } from "rethinkdb-ts";
 
 export default class Database {
     client: NucleusClient;
-    r = r;
-    pool: MasterPool | null = null
-
+    mongo: MongoClient
     constructor (client: NucleusClient) {
         this.client = client;
 
-        this.r.connectPool({db: "discord"})
-            .then(pool => this.pool = pool)
-            .catch(e => console.error(e));
+        this.mongo = new MongoClient(process.env.URI!);
     }
+    
+    // Helper Functions
+    async get(table: string,key?: string): Promise<unknown> {
+        const db = await this.mongo.connect();
+        const dbo = db.db(process.env.DATABASE);
 
-    get(table: string, key?: string) {
-        return key ? 
-            this.r
-                .table(table)
-                .get(key)
-                .run() :
-            this.r
-                .table(table)
-                .run();
-    }
+        const tab = await dbo.collection(table);
 
-    // eslint-disable-next-line @typescript-eslint/ban-types
-    insert(table: string, value: object = {}) {
-        return this.r
-            .table(table)
-            .insert(value)
-            .run();
-    }
-
-    // eslint-disable-next-line @typescript-eslint/ban-types
-    update(table: string, key: string, value: object = {}) {
-        return this.r
-            .table(table)
-            .get(key)
-            .update(value, { returnChanges: true })
-            .run();
-    }
-
-    delete (table: string, key: string) {
-        return this.r
-            .table(table)
-            .get(key)
-            .delete()
-            .run();
+        if (!key) return tab;
+        const val = tab.findOne({id: key});
+        return val;
     }
 }
