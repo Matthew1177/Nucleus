bbf360943ba1f7319d7fab0c0e62eb7259f24ac5
Matthew1177 <56777713+Matthew1177@users.noreply.github.com>
2020-08-30 15:00:34 -0700

    fix database - testing phase complete

--- src/lib/structures/Database.ts
+++ src/lib/structures/Database.ts
@@ -1,4 +1,4 @@
-import { MongoClient, FilterQuery, UpdateOneOptions } from "mongodb";
+import { MongoClient, FilterQuery, UpdateOneOptions, InsertOneWriteOpResult, UpdateWriteOpResult } from "mongodb";
 import NucleusClient from "../extensions/NucleusClient";
 
 export default class Database {
@@ -7,40 +7,39 @@ export default class Database {
     constructor (client: NucleusClient) {
         this.client = client;
 
-        this.mongo = new MongoClient(process.env.URI!);
+        this.mongo = new MongoClient(process.env.URI!,{ useUnifiedTopology: true });
     }
 
     // Helper Functions
-    async get(table: string,key?: string): Promise<unknown> {
+    async getOne(collection: string,filter: FilterQuery<unknown>): Promise<unknown> {
         const db = await this.mongo.connect();
         const dbo = db.db(process.env.DATABASE);
 
-        const tab = await dbo.collection(table);
+        const tab = dbo.collection(collection);
 
-        if (!key) return tab;
-        const val = tab.findOne({id: key});
+        const val = tab.findOne(filter);
         return val;
     }
 
     // It can be any value, does not matter
-    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-    async insert(table: string, obj: any): Promise<void> {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/explicit-module-boundary-types
+    async insertOne(collection: string, obj: any): Promise<InsertOneWriteOpResult<any>> {
         const db = await this.mongo.connect();
         const dbo = db.db(process.env.DATABASE);
 
-        const tab = dbo.collection(table);
+        const tab = dbo.collection(collection);
 
-        await tab.insertOne(obj);
-        return;
+        const ret = await tab.insertOne(obj);
+        return ret;
     }
 
-    async update(table: string, filter: FilterQuery<unknown>, update: UpdateOneOptions): Promise<void> {
+    async updateOne(collection: string, filter: FilterQuery<unknown>, update: UpdateOneOptions): Promise<UpdateWriteOpResult> {
         const db = await this.mongo.connect();
         const dbo = db.db(process.env.DATABASE);
 
-        const tab = dbo.collection(table);
+        const tab = dbo.collection(collection);
 
-        await tab.updateOne(filter,update);
-        return;
+        const ret = await tab.updateOne(filter,update);
+        return ret;
     }
 }
