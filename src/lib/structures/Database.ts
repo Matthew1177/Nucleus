bf79ca5cf96926ed4669d47ec067f74dda8cd26c
Matthew1177 <56777713+Matthew1177@users.noreply.github.com>
2020-09-08 22:11:07 -0700

    Update Database.ts

--- src/lib/structures/Database.ts
+++ src/lib/structures/Database.ts
@@ -1,11 +1,11 @@
 import {MongoClient} from 'mongodb';
 import NucleusClient from '../extensions/NucleusClient';
-import {Snowflake} from 'discord.js';
+import {Snowflake, Collection} from 'discord.js';
 
 class CacheRecord {
-  data: never;
+  data: Record<string, unknown>;
   creation: number;
-  constructor(data: never) {
+  constructor(data: Record<string, unknown>) {
     this.data = data;
     this.creation = Date.now();
   }
@@ -13,7 +13,7 @@ class CacheRecord {
 
 export default class Database {
   client: NucleusClient;
-
+  guildCache = new Collection<Snowflake, CacheRecord>();
   mongo: MongoClient | undefined;
 
   constructor(client: NucleusClient) {
@@ -27,17 +27,41 @@ export default class Database {
         useUnifiedTopology: true,
       }).connect();
     })();
+
+    setInterval(() => this.sweepCache(600), 300 * 1000);
   }
 
   // Helper Functions
-
-  async getGuild(id: Snowflake) {
+  async getGuild(id: Snowflake): Promise<Record<string, unknown> | null> {
+    const cacheItem = this.guildCache.get(id);
+    if (cacheItem) {
+      return cacheItem.data;
+    }
     const db = this.mongo!.db(process.env.DATABASE);
     const col = db.collection('guild_settings');
 
     const res = await col.findOne({id});
     if (res) {
-      //
+      this.guildCache.set(id, new CacheRecord(res));
     }
+    return res;
+  }
+
+  async insertGuild(obj: Record<string, unknown>): Promise<void> {
+    const db = this.mongo!.db(process.env.DATABASE);
+    const col = db.collection('guild_settings');
+
+    await col.insertOne(obj);
+  }
+
+  private sweepCache(lifetime: number): void {
+    if (lifetime <= 0) {
+      return;
+    }
+
+    const lifetimeMs = lifetime * 1000;
+    const now = Date.now();
+
+    this.guildCache.sweep(c => now - c.creation > lifetimeMs);
   }
 }
