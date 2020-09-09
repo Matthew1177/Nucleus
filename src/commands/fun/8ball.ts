942932ceedf5b710ef64505fcc14575999a3d0d5
Matthew1177 <56777713+Matthew1177@users.noreply.github.com>
2020-09-08 22:11:04 -0700

    Update 8ball.ts

--- src/commands/fun/8ball.ts
+++ src/commands/fun/8ball.ts
@@ -1,37 +1,37 @@
 import {Command} from '../../lib';
 import {MessageEmbed, Message} from 'discord.js';
-const responses = [
-  'It is certain.',
-  'It is decidedly so.',
-  'Without a doubt.',
-  'Yes – definitely.',
-  'You may rely on it.',
-  'As I see it, yes.',
-  'Most likely.',
-  'Outlook good.',
-  'Yes.',
-  'Signs point to yes.',
-  'Reply hazy, try again.',
-  'Ask again later.',
-  'Better not tell you now.',
-  'Cannot predict now.',
-  'Concentrate and ask again.',
-  "Don't count on it.",
-  'My reply is no.',
-  'My sources say no.',
-  'Outlook not so good.',
-  'Very doubtful.',
-];
 
 export default class extends Command {
   description = '';
   cooldown = 3;
+  private responses = [
+    'It is certain.',
+    'It is decidedly so.',
+    'Without a doubt.',
+    'Yes – definitely.',
+    'You may rely on it.',
+    'As I see it, yes.',
+    'Most likely.',
+    'Outlook good.',
+    'Yes.',
+    'Signs point to yes.',
+    'Reply hazy, try again.',
+    'Ask again later.',
+    'Better not tell you now.',
+    'Cannot predict now.',
+    'Concentrate and ask again.',
+    "Don't count on it.",
+    'My reply is no.',
+    'My sources say no.',
+    'Outlook not so good.',
+    'Very doubtful.',
+  ];
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   execute(message: Message, args: Array<string>): void {
-    const rand = Math.floor(Math.random() * responses.length);
+    const rand = Math.floor(Math.random() * this.responses.length);
     const embed = new MessageEmbed()
       .setColor(rand < 10 ? 0x5cb85c : rand < 15 ? 0xf0ad4e : 0xd9534f)
-      .setDescription(`${responses[rand]}`)
+      .setDescription(`${this.responses[rand]}`)
       .setAuthor(
         'Magic 8 Ball',
         'https://github.com/twitter/twemoji/blob/master/assets/72x72/1f3b1.png?raw=true'
