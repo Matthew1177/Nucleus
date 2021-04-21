dea6029062af145e7065cab8e928aa5016364a30
Matthew1177 <mekaireb@gmail.com>
2021-04-20 19:05:54 -0700

    fix naming

--- src/structures/NucleusPermissions.ts
+++ src/structures/NucleusPermissions.ts
@@ -8,13 +8,16 @@ type Permissions =
 
 type NucleusPermissionResolvable = BitFieldResolvable<Permissions>;
 
+const FLAGS = {
+  KICK_MEMBERS: 1 << 0,
+  BAN_MEMBERS: 1 << 1,
+  MUTE_MEMBERS: 1 << 2,
+  PURGE_MESSAGES: 1 << 3,
+};
+
 export default class NucleusPermissions extends BitField<Permissions> {
-  static FLAGS = {
-    KICK_MEMBERS: 1 << 0,
-    BAN_MEMBERS: 1 << 1,
-    MUTE_MEMBERS: 1 << 2,
-    PURGE_MESSAGES: 1 << 3,
-  };
+  static FLAGS = FLAGS;
+  static ALL = Object.values(FLAGS).reduce((all, p) => all | p, 0);
   constructor(bits: NucleusPermissionResolvable) {
     super(bits);
   }
