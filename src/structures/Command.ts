6a65b630c51cc3f5ec64b8632e1f3e4bac1c442e
Matthew1177 <mekaireb@gmail.com>
2021-04-21 22:06:44 -0700

    command fix

--- src/structures/Command.ts
+++ src/structures/Command.ts
@@ -1,17 +1,23 @@
 import {Client} from 'discord.js';
 import NucleusPermissions from './NucleusPermissions';
 
+export enum CommandContext {
+  DM_CHANNL,
+  GUILD_CHANNEL,
+  BOTH,
+}
+
 export default abstract class Command {
   client: Client;
   abstract name: string;
   aliases = [];
   premium = false;
   permissions: NucleusPermissions = new NucleusPermissions(0);
+  abstract context: CommandContext;
 
   constructor(client: Client) {
     this.client = client;
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   abstract execute(...args: unknown[]): void;
 }
