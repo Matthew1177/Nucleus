95691bb59d5f2356b0c1b81b9cc4e9dcf0287c91
Matthew1177 <mekaireb@gmail.com>
2021-04-21 22:38:41 -0700

    command handler getPrefix

--- src/events/CommandHandler.ts
+++ src/events/CommandHandler.ts
@@ -1,8 +1,34 @@
 import {Message} from 'discord.js';
+import NucleusGuild from '../extensions/NucleusGuild';
 import Event from '../structures/Event';
 
 export default class CommandHandler extends Event {
   name = 'message';
 
-  async execute(message: Message) {}
+  async execute(message: Message) {
+    if (!this.client.user) return;
+    if (message.author.bot) return;
+    const prefix = await this.getPrefix(message);
+    if (prefix !== undefined) {
+      const args = message.content.substring(prefix.length).split(' ');
+    }
+  }
+
+  private async getPrefix(message: Message): Promise<string | undefined> {
+    if (message.content.startsWith(`<@${this.client.user!.id}>`))
+      return `<@${this.client.user!.id}>`;
+    if (message.content.startsWith(`<@!${this.client.user!.id}>`))
+      return `<@!${this.client.user!.id}>`;
+
+    if (message.channel.type === 'dm') {
+      if (message.content.startsWith('!')) return '!';
+    } else {
+      const guild = message.guild! as NucleusGuild;
+      const prefixes = (await guild.fetchSettings()).prefixes;
+      for (const i in prefixes) {
+        if (message.content.startsWith(prefixes[i])) return prefixes[i];
+      }
+    }
+    return undefined;
+  }
 }
