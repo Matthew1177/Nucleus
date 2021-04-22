0d23a2575a95c5ba896808a859d96febb0a899df
Matthew1177 <mekaireb@gmail.com>
2021-04-21 22:05:53 -0700

    add pg to client

--- src/extensions/NucleusClient.ts
+++ src/extensions/NucleusClient.ts
@@ -1,7 +1,26 @@
 import {Client} from 'discord.js';
+import {Pool} from 'pg';
 import Event from '../structures/Event';
 
 export default class NucleusClient extends Client {
+  pool: Pool;
+
+  constructor() {
+    super({
+      ws: {
+        intents: [
+          'GUILDS',
+          'GUILD_MEMBERS',
+          'GUILD_BANS',
+          'GUILD_MESSAGES',
+          'DIRECT_MESSAGES',
+        ],
+      },
+    });
+
+    this.pool = new Pool();
+  }
+
   registerEvent(event: Event) {
     super[event.once ? 'once' : 'on'](event.name, event.execute);
   }
